ea875a4e592148c34ca72332f6a609f1d0b44716
Madison Vega <madison.r.vega@gmail.com>
2021-04-11 21:14:00 -0500

    added middleware to read static files

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
 // Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
