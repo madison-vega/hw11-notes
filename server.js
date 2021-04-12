f7eb850611a42a28fcef6109142643f967c119d0
Madison Vega <madison.r.vega@gmail.com>
2021-04-11 19:54:50 -0500

    added routes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
 
 
 
