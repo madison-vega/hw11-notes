3445b75c1a61419155974f3a55fc633e8a3e4c31
Madison Vega <madison.r.vega@gmail.com>
2021-04-11 19:53:37 -0500

    added server file, added dependencies, middleware

---
+++ server.js
@@ -0,0 +1,39 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Setup Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`App listening on http://localhost:" + PORT`));
+
