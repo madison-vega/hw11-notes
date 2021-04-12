a0ed29d846acf5677dc198ca2a46d9387db9ead9
Madison Vega <madison.r.vega@gmail.com>
2021-04-11 20:52:52 -0500

    added rest of routes and post function

--- server.js
+++ server.js
@@ -14,6 +14,22 @@ app.use(express.json());
 // Routes
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/api/notes', (req, res) => fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+}));
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        data.push(newNote);
+
+    });
+
+    
+
+    return res.json(newNote);
+
+})
 
 
 
