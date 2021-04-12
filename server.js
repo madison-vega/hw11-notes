9336725fef5127541c68b710fa1ec16219d87ffd
Madison Vega <madison.r.vega@gmail.com>
2021-04-11 21:08:59 -0500

    trying to fix issue:

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 app.get('/api/notes', (req, res) => fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
 }));
